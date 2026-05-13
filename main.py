53aed65562517eb31c7555cc200c0faea95a58b7
Mukesh <mukesh@Mukeshs-MacBook-Pro.local>
2026-05-13 21:30:28 +0530

    added local chat with history

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from dotenv import load_dotenv
+from gradio.monitoring_dashboard import demo
 from openai import OpenAI
 import os
+import gradio as gr
 
 load_dotenv(override=True)
 
-def main():
-    client = OpenAI(
-        api_key="not-needed",
-        base_url="http://localhost:11434/v1"
-    )
+MODEL = "gpt-4o-mini"
+LOCAL_MODEL = "llama3.1"
 
-    response = client.chat.completions.create(model="llama3.1", messages=[{"role": "user", "content": "Hi"}])
 
+def chat(message, history):
+    client = OpenAI(api_key="not applicable", base_url="http://localhost:11434/v1")
+
+    response = client.chat.completions.create(model=LOCAL_MODEL, messages=[
+        {"role": "user", "content": message}])
     print("Client initialized with API key.")
-    print(response.choices[0].message)
+
+    resp = response.choices[0].message.content;
+
+    print(response.choices[0].message.content)
+    return resp
+
+
+def main():
+    demo = gr.ChatInterface(fn=chat)
+    demo.launch()
 
 if __name__ == "__main__":
     main()
+
